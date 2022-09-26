ae52b6a13f4e200246ac3047c3992b45779a8ef7
Harshit Singhal <82471455+harry1121-s@users.noreply.github.com>
2022-09-26 15:06:33 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("GUI test")
+root.configure(background="yellow")
+root.minsize(200, 200)
+root.maxsize(500, 500)
+root.geometry("300x300+50+50")
+root.mainloop
